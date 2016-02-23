38109096ff3f6309db8ac2be24b44a06b760d1c5
Ashley Williams <ashley666ashley@gmail.com>
2016-02-23 17:03:43 -0500

    add express-handlebars, views, layout

--- app/index.js
+++ app/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const exphbs  = require('express-handlebars');
 const app = express();
 
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main', 
+  layoutsDir:'./app/views/layouts'
+}));
+
+app.set('views', './app/views');
+app.set('view engine', 'handlebars');
+
+const pickOne = require('pick-one');
+const motivations = require('motivations');
+
 app.get('/', function(request, response) {
-  var compliment = "hey i like your shoes"
-  response.send(compliment);
+  var motivation = pickOne(motivations);
+  response.render('motivation', { motivation });
 });
 
 module.exports = app;
